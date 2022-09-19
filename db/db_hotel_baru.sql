54409c0206b5700f5c7af3b8e65d9a18dea530fd
afiffarishudaifi <57697138+afiffarishudaifi@users.noreply.github.com>
2022-09-19 16:25:24 +0700

    upload database baru

---
+++ db/db_hotel_baru.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100413
+ Source Host           : localhost:3306
+ Source Schema         : db_hotel_baru
+
+ Target Server Type    : MySQL
+ Target Server Version : 100413
+ File Encoding         : 65001
+
+ Date: 19/09/2022 16:24:54
+*/
+
